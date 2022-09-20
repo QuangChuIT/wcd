23d14a659363ef9388a6dafec966671b68ae92f2
DESKTOP-L99JJ85\chuva <chuvanquang96@outlook.com>
2022-09-20 23:28:48 +0700

    push code

--- src/main/java/com/aptech/user/UserServlet.java
+++ src/main/java/com/aptech/user/UserServlet.java
@@ -1,4 +1,20 @@
 package com.aptech.user;
 
-public class UserServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/views/user/users.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
